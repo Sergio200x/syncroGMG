b206fe4f66fbaed3935f617f4a8e6e0010262071
Sergio200x <sergio_200x@hotmail.com>
2022-10-30 19:41:14 -0300

    agregando desrcipcion de pais en query

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,4 +1,3 @@
 export const querys ={
 
-    Locales_not_sync:"USE EMPRESAS select sCod_Local as 'Codigo_de_local',horas from ( select  sCod_Local,DATEDIFF(hour,dFecha_Actividad,GETDATE()) horas   from Transfer_UDL  where   DATEDIFF(hour,dFecha_Actividad,GETDATE())>6 and DATEDIFF(hour,dFecha_Actividad,GETDATE())<100 and sCod_Local not in ('')) tabla"
-}
+    Locales_not_sync:"use EMPRESAS select sCod_Local as 'Codigo_de_local', ('GMG'+'   '+[Nombre_local]) AS 'Nombre_local' ,horas from (select sCod_Local,DATEDIFF(hour,dFecha_Actividad,GETDATE()) horas, m.local_nombre as [Nombre_local]   from Transfer_UDL AS T left JOIN GMG_ERP.DBO.LOCALES AS M ON M.local_codigo=T.sCod_Local collate SQL_Latin1_General_CP1_CI_AS where sCod_Local not in ('FLF2','FMLAN_O','PEÑ','FCSJ3_','MSA','M3F','PRU','') and DATEDIFF(hour,dFecha_Actividad,GETDATE())>6 AND DATEDIFF(hour,dFecha_Actividad,GETDATE())<150) tabla"}
